afff150c304b09b2ef0f32d91b261f1c944ccb90
henry wei <henry.wh.wei@gmail.com>
2024-07-30 13:32:13 +0800

    some changes

--- src/botcontrol.cpp
+++ src/botcontrol.cpp
@@ -22,21 +22,7 @@ bool x;
 bool y;
 
 void driver(){
-  //  resetPuncher();
-   
-
   while(1){
-    // otherweirdstuff();
-    // double turnVal = curveJoystick(false, con.Axis1.position(percent), turningCurve); //Get curvature according to settings [-100,100]
-    // double forwardVal = curveJoystick(false, con.Axis3.position(percent), forwardCurve); //Get curvature according to settings [-100,100]
-
-    // double turnVolts = -turnVal * 0.12; //Converts to voltage
-    // double forwardVolts = forwardVal * 0.12; //Converts to voltage
-
-    // leftmo.spin(forward, forwardVolts + turnVolts, voltageUnits::volt); //Apply Via Voltage
-    // rightmo.spin(forward, forwardVolts - turnVolts, voltageUnits::volt);
-
-    // vex::task::sleep(20);
     if(con.ButtonR1.pressing()){
       intas.spin(reverse, 100, pct);
     } else if(con.ButtonR2.pressing()){
@@ -44,21 +30,12 @@ void driver(){
     } else{
       intas.stop(hold);
     }
-    // if(con.ButtonRight.pressing()){
-    //   smlboi.spin(reverse, 100, pct);
-    //   ptoboi.spin(reverse, 100, pct);
-    // } else if(con.ButtonLeft.pressing()){
-    //   smlboi.spin(fwd, 100, pct);
-    //   ptoboi.spin(fwd, 100, pct);
-    // } else{
-    //   smlboi.stop(hold);
-    //   ptoboi.stop(hold);
-    // }
+
 /////////////////////////////////////////////////////////////////
 
-    double axis3 = -con.Axis3.position(pct)*0.5;
-    double axis1 = -con.Axis1.position(pct)*0.7;    
-     double leftVolt = axis3 - axis1;
+    double axis3 = con.Axis3.position(pct);
+    double axis1 = con.Axis1.position(pct);    
+    double leftVolt = axis3 - axis1;
     double rightVolt = axis3 + axis1;
     double scale = 12.0 / fmax(12.0, fmax(fabs(leftVolt), fabs(rightVolt)));
     leftVolt *= scale;
@@ -83,8 +60,6 @@ void driver(){
 
 
 ////////////////////////////////////////////////////////////////////////////////
-    // double xxxx = (leftmo.temperature(celsius)+rightmo.temperature(celsius))/2;
-    // Brain.Screen.print(xxxx);
 
   if(clrsr.color()==blue){
       wrgclr.set(1);
